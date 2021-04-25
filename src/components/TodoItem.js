1b149a8f5a8b7048d2d2f25493fec28b28ec9e40
Your Name <you@example.com>
2021-04-24 21:55:16 -0500

    every original archives

---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TodoItem(props){
+  
+  const completedStyle={
+    fontStyle:"italic",
+    color:"#cdcdcd",
+    textDecoration:"line-through"
+  }
+
+  return(
+    <div className="todo-item">
+        <input 
+        type="checkbox" 
+        defaultChecked={props.myitem.completed}
+        onChange={() => props.handleChanges(props.myitem.id)}
+        />
+        <p style={props.myitem.completed? completedStyle: null}>{props.myitem.task}</p>
+    </div>
+ )
+}
+export default TodoItem
---
+++ src/todosData.js
@@ -0,0 +1,28 @@
+const todosData=[
+{
+    id:1,
+    task:"Meditate",
+    completed:false
+},
+{
+    id:2,
+    task:"Take out the trash",
+    completed:false
+},
+{
+    id:3,
+    task:"Take a cup of matcha tea",
+    completed:false
+},
+{
+    id:4,
+    task:"Do my bed",
+    completed:false
+},
+{
+    id:5,
+    task:"Trading session 8 am",
+    completed:false
+}
+]
+export default todosData
