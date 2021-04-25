b3c48cabefb02b09278781c419ff555fb1fe4c47
Your Name <you@example.com>
2021-04-24 23:34:34 -0500

    kjcbfjñf

--- src/todosData.js
+++ src/todosData.js
@@ -23,6 +23,32 @@ const todosData=[
         id:5,
         task:"embrace mom",
         completed:false
+    },
+    {
+        id:6,
+        task:"Be happy",
+        completed:false
+    },
+    {
+        id:7,
+        task:"Sing",
+        completed:false
+    },
+    {
+        id:8,
+        task:"Take a cup of cofee or tea",
+        completed:false
+    },
+    {
+        id:9,
+        task:"Do my bed",
+        completed:false
+    },
+    {
+        id:10,
+        task:"embrace mom",
+        completed:false
     }
+    
     ]
     export default todosData
